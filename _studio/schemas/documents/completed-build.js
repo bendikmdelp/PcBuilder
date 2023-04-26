4f18bae67992cbc7bae3334e6607049cb4a9077d
WaffleChampion <bendikmdelp@gmail.com>
2023-04-26 14:20:10 +0200

    added completed build to sanity

--- _studio/schemas/documents/completed-build.js
+++ _studio/schemas/documents/completed-build.js
@@ -1,41 +1,64 @@
-// export default {
-// 	title: 'Completed Build',
-// 	name: 'completedBuild',
-// 	type: 'document',
-// 	fields: [
-// 		{
-// 			title: 'Name',
-// 			name: 'name',
-// 			type: 'string',
-// 		},
-// 		{
-// 			title: 'Slug',
-// 			name: 'slug',
-// 			type: 'slug',
-// 			options: {
-// 				source: 'name'
-// 			}
-// 		},
-// 		{
-// 			title: 'CPU',
-// 			name: 'CPU',
-// 			type: 'reference',
-// 			to: { type: 'product' },
-// 			options: {
-// 				filter: 'product == $product',
-// 				filterParams: {product: 'CPU'}
-// 			}
-// 		},
-// 		{
-// 			title: 'Motherboard',
-// 			name: 'motherboard',
-// 			type: 'reference',
-// 			to: { type: 'product' },
-// 			options: {
-// 				filter: 'product == $product',
-// 				filterParams: {product: 'motherboard'}
-// 			}
-// 		},
+export default {
+	title: 'Completed Build',
+	name: 'completedBuild',
+	type: 'document',
+	fields: [
+		{
+			title: 'Name',
+			name: 'name',
+			type: 'string',
+		},
+		{
+			title: 'Slug',
+			name: 'slug',
+			type: 'slug',
+			options: {
+				source: 'name'
+			}
+		},
+		{
+			title: 'CPU',
+			name: 'CPU',
+			type: 'reference',
+			to: { type: 'product' },
+		},
+		{
+			title: 'Motherboard',
+			name: 'motherboard',
+			type: 'reference',
+			to: { type: 'product' },
+		},
+		{
+			title: 'Cooler',
+			name: 'cooler',
+			type: 'reference',
+			to: { type: 'product' },
+		},
+		{
+			title: 'Memory',
+			name: 'memory',
+			type: 'reference',
+			to: { type: 'product' },
+		},
+		{
+			title: 'Gpu',
+			name: 'gpu',
+			type: 'reference',
+			to: { type: 'product' },
+		},
+		{
+			title: 'Power supply',
+			name: 'powerSupply',
+			type: 'reference',
+			to: { type: 'product' },
+		},
+		{
+			title: 'Case',
+			name: 'case',
+			type: 'reference',
+			to: { type: 'product' },
+		}
+		
 
-// 	]
-// }
+	]
+}
