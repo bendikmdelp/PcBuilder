a95315658758faadba91cabf4d48d644e92d2911
WaffleChampion <bendikmdelp@gmail.com>
2023-05-04 14:02:27 +0200

    updated query

--- _app/js/util/get-from-db.js
+++ _app/js/util/get-from-db.js
@@ -2,10 +2,37 @@ import {sanity} from "../sanity.js"
 
 export async function getProducts() {
 	const query = `*[_type == "product"] {
-		...,
-		"images": images[].asset -> url,
-		"manufacturer": manufacturer -> name,
-		"category": category -> name
+		*[_type == "product"] {
+			name,
+			"images": images[].asset -> url,
+			"manufacturer": manufacturer -> name,
+			"category": category -> name,
+			price,
+			cores,
+			threads,
+			coreClock,
+			socket,
+			coolerSocket,
+			watercooled,
+			coolerheight,
+			motherboardSocket,
+			memoryType,
+			wifi,
+			RAMType,
+			RAMCapacity,
+			storageType,
+			storageCapacity,
+			GPUChipset,
+			VRAM,
+			coreClock,
+			GPUMemoryType,
+			efficencyRating,
+			wattage,
+			modular,
+			caseType,
+			sidePanel,
+			colour
+		 }
 	 }`
 
 	const products = await sanity.fetch(query)
