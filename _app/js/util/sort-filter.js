126a9cc83771db80f83ac4b9d0388590876de54e
WaffleChampion <bendikmdelp@gmail.com>
2023-05-09 13:01:32 +0200

    created util for sorting and filtering

---
+++ _app/js/util/sort-filter.js
@@ -0,0 +1,39 @@
+export function getObjectKeys(object) {
+	return Object.getOwnPropertyNames(object);
+}
+
+export function getClickedCategoryObjectKeys(products) {
+	let newArray = []
+
+
+	for(const product of products) {
+		let newObject = {};
+		for(const [key, value] of Object.entries(product)){
+			if(value){
+				if(key !== 'images' &&
+				key !== 'manufacturer' &&
+				key !== 'price' &&
+				key !== 'category' &&
+				key !== 'name'){
+					Object.defineProperty(newObject, key, {
+						value: value,
+						writable: true
+					})
+				};
+				
+			};
+		};
+		newArray.push(newObject)
+	}
+	return newArray;
+}
+
+export function getLowestPrice(products) {
+	const getLowest = Math.min(...products.map(item => item.price));
+	return getLowest/100
+}
+
+export function getHighestPrice(products) {
+	const getHighest = Math.max(...products.map(item => item.price));
+	return getHighest/100
+}
