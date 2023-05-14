a48588de0666c65fa20e82f5aa2b0ecc1077e517
WaffleChampion <bendikmdelp@gmail.com>
2023-05-14 14:07:19 +0200

    added navigate on click to add component

--- _app/js/modules/builder.js
+++ _app/js/modules/builder.js
@@ -1,6 +1,25 @@
 import { getProducts } from "../util/get-from-db.js";
 
 export default async function builder() {
-	const products = await getProducts();
+	const addComponentButtons = document.querySelectorAll('.main-container__add-selection-button');
+
+	addComponentButtons.forEach(element => {
+		element.addEventListener('click', handleAddComponentButtonClick);
+	})
+
+
+	function handleAddComponentButtonClick(event) {
+		saveClickedComponentToLocalStorage(event);
+		navigateToBrowseProductsPage();
+	}
+
+	function saveClickedComponentToLocalStorage(event) {
+		const clickedComponent = event.currentTarget.dataset.component;
+		localStorage.setItem('clickedComponent', clickedComponent);
+	}
+
+	function navigateToBrowseProductsPage() {
+		window.location.href = "/_app/browse-products";
+	}
 
 }
