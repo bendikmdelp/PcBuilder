3d8167646351687eef0ba083d344f43e6958b2a9
WaffleChampion <bendikmdelp@gmail.com>
2023-05-18 12:45:26 +0200

    create add product to storage with duplicate check

--- _app/js/modules/builder.js
+++ _app/js/modules/builder.js
@@ -1,16 +1,26 @@
 import { getProducts } from "../util/get-from-db.js";
 
 export default async function builder() {
+	const productsAddedToBuilder = getProductsFromLocalStorage();
+
 	const addComponentButtons = document.querySelectorAll('.main-container__add-selection-button');
 
 	addComponentButtons.forEach(element => {
 		element.addEventListener('click', handleAddComponentButtonClick);
-	})
-
+	});
+	handlePageLoad();
 
 	function handleAddComponentButtonClick(event) {
 		saveClickedComponentToLocalStorage(event);
 		navigateToBrowseProductsPage();
+	}
+
+	function handlePageLoad() {
+		if(productsAddedToBuilder) {
+			renderHTML();
+		} else {
+			//console.log('nothing added')
+		}
 	}
 
 	function saveClickedComponentToLocalStorage(event) {
@@ -20,6 +30,14 @@ export default async function builder() {
 
 	function navigateToBrowseProductsPage() {
 		window.location.href = "/_app/browse-products";
+	}
+
+	function getProductsFromLocalStorage() {
+		if(localStorage.getItem('products')) {
+			return localStorage.getItem('products');
+		}else {
+			return null;
+		}
 	}
 
 }
