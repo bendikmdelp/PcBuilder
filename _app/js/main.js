eb157d30f6faa4693464df5e14391d2ae338b555
WaffleChampion <bendikmdelp@gmail.com>
2023-05-23 13:16:11 +0200

    added random product names to frontpage

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,7 +1,9 @@
 import {browseProducts} from "./modules/browse-products.js";
 import builder from "./modules/builder.js";
+import frontPage from "./modules/front-page.js";
 import navigateToPage from "./modules/navigate-to-page.js";
 
+frontPage();
 builder();
 navigateToPage();
 browseProducts();
--- _app/js/modules/front-page.js
+++ _app/js/modules/front-page.js
@@ -22,13 +22,61 @@ export default function frontPage() {
 		renderHTML('builds');
 	}
 
+	function getRandomProductNames() {
+		const products = productList;
+		const randomIndex = getRandomIndexes(products)
+		const productNames = getProductNames(randomIndex, products);
+		return productNames;
+	}
+
+	function getRandomIndexes(products) {
+		let array = []
+		generateTenRandomNubers(products, array, 0);
+		return array;
+	}
+
+	function generateTenRandomNubers(products, array, index) {
+		for(index; array.length < 10; index++) {
+			let number = getRandomNumber(products);
+			if(array.includes(number)) {
+				generateTenRandomNubers(products, array, index);
+			} else {
+				array.push(number);
+			}
+		}
+	}
+
+	function getRandomNumber(products) {
+		return Math.floor(Math.random() * ((products.length-1) - 0 +1) + 0);
+	}
+
+	function getProductNames(index, products) {
+		let productNames = []
+		for(const item of index) {
+			productNames.push(products[item].name);
+		}
+
+		return productNames;
+	}
+
+	function renderMostPopularProducts(randomProductNames) {
+		const products = randomProductNames;
+		for(const [index, product] of products.entries()) {
+			const listItem = document.createElement('li');
+			listItem.className = 'popular-products__list-item';
+			listItem.innerText = `${index+1}: ${product}`;
+			mostPopularProductsList.append(listItem);
+		}
+	}
+
 	function renderHTML(defined) {
 		if(defined === 'products') {
 			const randomProductNames = getRandomProductNames();
 			renderMostPopularProducts(randomProductNames);
-		} else if (defined === 'builds') {
-			const randomCompletedBuilds = getRandomCompletedBuilds();
-			renderCompletedBuilds(randomCompletedBuilds);
 		}
+		// } else if (defined === 'builds') {
+		// 	const randomCompletedBuilds = getRandomCompletedBuilds();
+		// 	renderCompletedBuilds(randomCompletedBuilds);
+		// }
 	}
 }
