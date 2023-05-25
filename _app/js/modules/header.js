68a57e6af2306410bd0ebc3382617bd604c2cd02
WaffleChampion <bendikmdelp@gmail.com>
2023-05-25 10:55:58 +0200

    added mobile styling to header

---
+++ _app/js/modules/header.js
@@ -0,0 +1,20 @@
+export default function header() {
+	const mobileNavigationButton = document.querySelector('.header-mobile__navigation-button');
+	const mobileNavigationButtonContainer = document.querySelector('.header-mobile__button-container');
+
+	mobileNavigationButton.addEventListener('click', handleMobileNavigationButtonClick);
+
+	function handleMobileNavigationButtonClick() {
+		displayMobileNavigationButtonContainer()
+	}
+
+	function displayMobileNavigationButtonContainer() {
+		const elementStyle = getComputedStyle(mobileNavigationButtonContainer);
+		if(elementStyle.display === 'flex') {
+			mobileNavigationButtonContainer.style.display = 'none'
+		} else {
+			mobileNavigationButtonContainer.style.display = 'flex';
+		}
+
+	}
+}
