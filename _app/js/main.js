ca25772144776ff23a9e78bdd18640023f0ed269
Bendik Matheussen Delp <bendikmdelp@gmail.com>
2023-05-25 12:58:24 +0200

    Feature/mobile styling (#8)
    * added mobile styling to header
    
    * fixed navigation on mobile
    
    * updated sanity
    
    * added flexdirection column on mobile
    
    * removed unecessary code
    
    * fixed frontpage fontsize on mobile
    
    * fixed footer styling mobile
    
    * added try catch to get from db
    
    * fixed mobile layout in builder
    
    * fixed browse products mobile
    
    * fixed displaying info browse products mobile

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,8 +1,10 @@
 import {browseProducts} from "./modules/browse-products.js";
 import builder from "./modules/builder.js";
 import frontPage from "./modules/front-page.js";
+import header from "./modules/header.js";
 import navigateToPage from "./modules/navigate-to-page.js";
 
+header();
 frontPage();
 builder();
 navigateToPage();
