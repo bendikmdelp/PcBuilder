0dbc1d226da91aa62cf07ab828cf2e64dbfe8c29
WaffleChampion <bendikmdelp@gmail.com>
2023-05-29 14:03:40 +0200

    added comments to completed builds

--- _app/js/modules/completed-builds.js
+++ _app/js/modules/completed-builds.js
@@ -4,14 +4,21 @@ import { getCompletedBuilds } from "../util/get-from-db.js";
 const builds = await getCompletedBuilds();
 
 export default function completedBuilds() {
+	//Queryselector
 	const completedBuildsContainer = document.querySelector('.completed-builds__builds-container');	
 
-	onload()
-
-	function onload() {
-		renderHTML()
+	//runs on page load
+	if(completedBuildsContainer) {
+		onload();
 	}
 
+
+	//function to run on page load
+	function onload() {
+		renderHTML();
+	}
+
+	//goes through each element in builds to render each
 	function renderCompletedBuildList() {
 		for(const build of builds) {
 			const buildCard = createBuildFigureDOM(build);
@@ -19,6 +26,7 @@ export default function completedBuilds() {
 		}
 	}
 
+	//renderes completed builds
 	function renderHTML() {
 		renderCompletedBuildList();
 	}
