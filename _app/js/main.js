93eecbb00e5661e322755f0c22665980d369e8e4
Bendik Matheussen Delp <bendikmdelp@gmail.com>
2023-05-29 11:37:47 +0200

    added completed builds to main (#11)

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,6 @@
 import {browseProducts} from "./modules/browse-products.js";
 import builder from "./modules/builder.js";
+import completedBuilds from "./modules/completed-builds.js";
 import frontPage from "./modules/front-page.js";
 import header from "./modules/header.js";
 import navigateToPage from "./modules/navigate-to-page.js";
@@ -9,3 +10,4 @@ frontPage();
 builder();
 navigateToPage();
 browseProducts();
+completedBuilds();
